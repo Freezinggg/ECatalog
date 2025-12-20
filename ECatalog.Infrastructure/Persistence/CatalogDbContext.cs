d9b6ea9666489206bef8914839f5d8f21052e118
Freezinggg <blerubs@gmail.com>
2025-12-20 13:45:23 +0700

    Add project files.

---
+++ ECatalog.Infrastructure/Persistence/CatalogDbContext.cs
@@ -0,0 +1,31 @@
+﻿using ECatalog.Domain.Entity;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECatalog.Infrastructure.Persistence
+{
+    public class CatalogDbContext : DbContext
+    {
+        public CatalogDbContext(DbContextOptions<CatalogDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<CatalogItem> CatalogItems { get; set; } = null!;
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<CatalogItem>(entity =>
+            {
+                entity.HasKey(x => x.Id);
+                entity.Property(x => x.Id).IsRequired();
+                entity.Property(x => x.Name).IsRequired();
+                entity.Property(x => x.Description).IsRequired();
+                entity.Property(x => x.Price).IsRequired();
+            });
+        }
+    }
+}
---
+++ ECatalog.Infrastructure/Persistence/CatalogDbContextFactory.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECatalog.Infrastructure.Persistence
+{
+    public class CatalogDbContextFactory
+     : IDesignTimeDbContextFactory<CatalogDbContext>
+    {
+        public CatalogDbContext CreateDbContext(string[] args)
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<CatalogDbContext>();
+
+            // DESIGN-TIME connection string
+            optionsBuilder.UseNpgsql(
+                "Host=localhost;Port=5432;Database=ecatalog;Username=postgres;Password=password");
+
+            return new CatalogDbContext(optionsBuilder.Options);
+        }
+    }
+}
